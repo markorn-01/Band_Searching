907e38ca6a44f4746ced0f7ca6e00513dc75486c
Minh Ngo <80674608+markorn-01@users.noreply.github.com>
2023-02-08 12:08:42 +0700

    Add searching while loop

--- query.java
+++ query.java
@@ -27,19 +27,27 @@ public class query {
             System.out.println("Instancize Statement...");
             stmt = conn.createStatement();
             String sql, searchName;
-            System.out.println("Enter the name of the band you want to search for: "); 
-            searchName = sc.nextLine();
-            sql = "SELECT a.name AS 'Album Name',  a.release_year AS 'Release Year', s.name AS 'Song Name' FROM albums a JOIN songs s ON a.id = s.album_id JOIN bands b ON a.band_id = b.id WHERE b.name = '" + searchName + "'GROUP BY a.name, a.release_year, s.name ORDER BY a.name;";
-            ResultSet rs = stmt.executeQuery(sql);
-            // Query results
-            System.out.printf("%-50s%-25s%s\n","Album Name","Release Year","Song Name");
-            while (rs.next()) {
-                // The field type needs to be specified
-                String name      = rs.getString("Album Name");
-                int    release_year = rs.getInt("Release Year");
-                String song_name = rs.getString("Song Name");
-                // Print information
-                System.out.printf("%-50s%-25d%-50s\n", name, release_year, song_name);
+            ResultSet rs;
+            while (true){
+                System.out.println("Enter the name of the band you want to search for: "); 
+                searchName = sc.nextLine();
+                sql = "SELECT a.name AS 'Album Name',  a.release_year AS 'Release Year', s.name AS 'Song Name' FROM albums a JOIN songs s ON a.id = s.album_id JOIN bands b ON a.band_id = b.id WHERE b.name = '" + searchName + "'GROUP BY a.name, a.release_year, s.name ORDER BY a.name;";
+                rs = stmt.executeQuery(sql);
+                // Query results
+                System.out.printf("%-50s%-25s%s\n","Album Name","Release Year","Song Name");
+                while (rs.next()) {
+                    // The field type needs to be specified
+                    String name      = rs.getString("Album Name");
+                    int    release_year = rs.getInt("Release Year");
+                    String song_name = rs.getString("Song Name");
+                    // Print information
+                    System.out.printf("%-50s%-25d%-50s\n", name, release_year, song_name);
+                }
+                System.out.println("Do you want to search again? (Y/N)");
+                String answer = sc.nextLine();
+                if (answer.equals("N") || answer.equals("n")){
+                    break;
+                }
             }
             // Disconnect database
             rs.close();
